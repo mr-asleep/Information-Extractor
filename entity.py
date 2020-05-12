781d252a36845f23ba8d992e5ae01258439e497d
mr-asleep <51784622+mr-asleep@users.noreply.github.com>
2020-05-12 22:00:31 +0530

    Update entity.py

--- entity.py
+++ entity.py
@@ -12,11 +12,35 @@ def words_c(A):
 		return str(A)
 
 def output(response): # look at textrazor's tutorial for adding entity, relations, etc.
-	phrases=[]
+	dic={}
+	for x in response.entities():
+		try:
+			dic['Entity'].append(words_c(x.id))
+		except:
+			dic['Entity']=[]
+			dic['Entity'].append(words_c(x.id))
+
+	for x in response.relations():
+	    try:
+	    	dic['Relation'].append(words_c(x.predicate_words))
+	    except:
+	    	dic['Relation']=[]
+	    	dic['Relation'].append(words_c(x.predicate_words))
+
 	for x in response.noun_phrases():
-		# print(x)
-		phrases.append(words_c(x._words))
-	return phrases
+	    try:
+	    	dic['Noun Phrase'].append(words_c(x._words))
+	    except:
+	    	dic['Noun Phrase']=[]
+	    	dic['Noun Phrase'].append(words_c(x._words))
+
+	for x in response.properties():
+		try:
+			dic['Property'].append(words_c(x.predicate_words))
+		except:
+			dic['Property']=[]
+			dic['Property'].append(words_c(x.predicate_words))
+	return dic
 
 text="Iron Man is Robert Downey Jr. Barack Obama is the president of America."
 client = TextRazor('a02e9cce51c6c087f150269a4eab62812057696716b70eb1f2e0a8fd', extractors=["entities","phrases"])
